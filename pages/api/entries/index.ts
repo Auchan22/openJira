9e82a20b7a6ce501dd19502f74b3fe8a98269e83
Agustin <agustinsurila@gmail.com>
2022-12-20 20:52:43 -0300

    Clase 131: Creando metodo POST

--- pages/api/entries/index.ts
+++ pages/api/entries/index.ts
@@ -17,11 +17,12 @@ export default function handler(
     case 'GET':
       return getEntries(res);
 
+    case 'POST':
+      return createEntry(req, res);
+
     default:
       return res.status(400).json({ name: 'Endpoint no existe' });
   }
-
-  res.status(200).json({ name: 'Example' });
 }
 
 const getEntries = async (res: NextApiResponse<Data>) => {
@@ -32,4 +33,27 @@ const getEntries = async (res: NextApiResponse<Data>) => {
   await db.disconnect();
 
   res.status(200).json(entries);
+};
+
+const createEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { description } = req.body;
+
+  const newEntry = new Entry({
+    description,
+    createdAt: Date.now(),
+  });
+
+  try {
+    await db.connect();
+
+    await newEntry.save();
+
+    await db.disconnect();
+
+    return res.status(201).json(newEntry);
+  } catch (error) {
+    await db.disconnect();
+    console.log(error);
+    return res.status(500).json({ name: 'Revisar consola de servidor :(' });
+  }
 };
