f95aec097ad194600e7fa75a3e27c5a2264d8696
Agustin <agustinsurila@gmail.com>
2022-12-20 19:12:37 -0300

    Clase 124: Configurando conexion a DB

--- pages/api/seed.ts
+++ pages/api/seed.ts
@@ -1,15 +1,12 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  ok: boolean;
   name: string;
-  method?: string;
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ ok: true, name: 'John Doe', method: req.method });
+  res.status(200).json({ name: 'Example' });
 }
