3f1a728c0a872848f09efbe4efc4600deb243600
Agustin <agustinsurila@gmail.com>
2022-12-21 17:11:57 -0300

    Clase 133: Endpoint para update

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { db } from '../../../database';
+import { Entry, IEntry } from '../../../models';
+
+type Data =
+  | {
+      message: string;
+    }
+  | IEntry;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const { id } = req.query;
+
+  if (!mongoose.isValidObjectId(id)) {
+    return res.status(400).json({ message: 'Invalid ID: ' + id });
+  }
+
+  switch (req.method) {
+    case 'PUT':
+      return updateEntry(req, res);
+
+    default:
+      return res.status(400).json({ message: 'Invalid ID: ' + id });
+  }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { id } = req.query;
+  await db.connect();
+  const entryUpdate = await Entry.findById(id);
+  if (!entryUpdate) {
+    return res
+      .status(400)
+      .json({ message: 'No se encontro la entry con ese id' });
+  }
+
+  const { description = entryUpdate.description, status = entryUpdate.status } =
+    req.body;
+
+  const updatedEntry = await Entry.findByIdAndUpdate(
+    id,
+    {
+      description,
+      status,
+    },
+    { runValidators: true, new: true },
+  );
+
+  res.status(200).json(updatedEntry);
+};
